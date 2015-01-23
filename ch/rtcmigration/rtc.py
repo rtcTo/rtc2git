263f9ef11fd60d037f5e757990ce7a03ac8ea8b7
Manuel <wtfjoke@livenet.ch>
2015-01-23 01:02:17 +0100

    added support of comparing with streams / replaced lscm with scm since execution of lscm throws a filenotfound error

--- ch/rtcmigration/rtc.py
+++ ch/rtcmigration/rtc.py
@@ -1,20 +1,25 @@
 from rtc2git.ch.rtcmigration.commons import Shell
-from rtc2git.ch.rtcmigration import rtcmigration
 
 
 class ImportHandler:
     dateFormat = "yyyy-MM-dd" + Shell.spaceSeparator + "HH:mm:ss"
     informationSeparator = "@@"
+    workspace = None
+    repositoryURL = None
+    outputFileName = "output.txt"
 
-    def compareWithBaseLine(baseLineToCompare):
+    def __init__(self, workspace, repository):
+        self.workspace = workspace
+        self.repositoryURL = repository
+
+
+    def getChangeEntries(self, baselineToCompare):
         Shell.execute(
-            "lscm --show-alias n --show-uuid y compare ws " + rtcmigration.workspace + " baseline " + baseLineToCompare + " -r " + rtcmigration.repositoryURL + " -I sw -C @@{name}@@ --flow-directions i -D @@\"" + ImportHandler.dateFormat + "\"@@")
-
-    def getChangeEntriesFromCompareFile(noarg):
+            "scm --show-alias n --show-uuid y compare ws " + self.workspace + " baseline " + baselineToCompare + " -r " + self.repositoryURL + " -I sw -C @@{name}@@ --flow-directions i -D @@\"" + self.dateFormat + "\"@@")
         changeEntries = []
-        with open(rtcmigration.outputFileName, 'r') as file:
+        with open(self.outputFileName, 'r') as file:
             for line in file:
-                splittedLines = line.split(ImportHandler.informationSeparator)
+                splittedLines = line.split(self.informationSeparator)
                 revisionWithBrackets = splittedLines[0].strip()
                 revision = revisionWithBrackets[1:-1]
                 author = splittedLines[1].strip()
@@ -25,20 +30,53 @@ class ImportHandler:
                 # print("Revision: " + revision + " Author:" + author + " Comment" + comment + " Date: " +date)
         return changeEntries
 
-    def acceptChangesIntoWorkspace(baselineToCompare):
-        ImportHandler.compareWithBaseLine(baselineToCompare)
-        changeEntries = ImportHandler.getChangeEntriesFromCompareFile("")
+    def acceptChangesIntoWorkspace(self, baselineToCompare):
+        changeEntries = self.getChangeEntries(baselineToCompare)
         for changeEntry in changeEntries:
             revision = changeEntry.revision
-            print("accepting: " + changeEntry.comment + " (rev: " + revision + ")")
+            print("accepting: " + changeEntry.comment + " (Date: " + changeEntry.date, " Revision: " + revision + ")")
             Shell.execute(
-                "lscm accept --changes " + revision + " -r " + rtcmigration.repositoryURL + " --target " + rtcmigration.workspace,
+                "scm accept --changes " + revision + " -r " + self.repositoryURL + " --target " + self.workspace,
                 "accept.txt", "a")
             print("Revision " + revision + " accepted")
         print("All changes from " + baselineToCompare + " accepted")
 
-    def acceptChangesFromBaseLine(baseLineToCompare):
-        ImportHandler.acceptChangesIntoWorkspace(baseLineToCompare)
+    def acceptChangesFromBaseLine(self, baseLineToCompare):
+        self.acceptChangesIntoWorkspace(baseLineToCompare)
+
+    def getBaseLinesFromStream(self, stream, filename):
+        skipFirstRow = 1
+        isComponentLine = 2
+        componentBaseLinesEntries = []
+        component = None
+        baseline = None
+        with open(filename, 'r') as file:
+            for line in file:
+                if (skipFirstRow == 1):
+                    skipFirstRow = 0
+                    continue
+                splittedLines = line.split("\"")[0].split(" ")
+                if isComponentLine % 2 == 0:
+                    component = splittedLines[3].strip()[1:-1]
+                else:
+                    baseline = splittedLines[5].strip()[1:-1]
+
+                if baseline != None and component != None:
+                    componentBaseLineEntry = ComponentBaseLineEntry(component, baseline)
+                    componentBaseLinesEntries.append(componentBaseLineEntry)
+                    baseline = None
+                    component = None
+
+                isComponentLine += 1
+        return componentBaseLinesEntries
+
+    def acceptChangesFromStreams(self, streams):
+        for stream in streams:
+            fileName = "StreamComponents_" + stream + ".txt"
+            Shell.execute("scm --show-alias n --show-uuid y list components -v -r " + self.repositoryURL + " " + stream,
+                          fileName)
+            for componentBaseLineEntry in self.getBaseLinesFromStream(stream, fileName):
+                self.acceptChangesFromBaseLine(componentBaseLineEntry.baseline)
 
 
 class ChangeEntry:
@@ -52,3 +90,12 @@ class ChangeEntry:
         self.author = author
         self.date = date
         self.comment = comment
+
+
+class ComponentBaseLineEntry:
+    component = None
+    baseline = None
+
+    def __init__(self, component, baseline):
+        self.component = component
+        self.baseline = baseline
--- ch/rtcmigration/rtcmigration.py
+++ ch/rtcmigration/rtcmigration.py
@@ -1,6 +1,7 @@
 import configparser
 
 from rtc2git.ch.rtcmigration.commons import Shell
+from rtc2git.ch.rtcmigration.rtc import ImportHandler
 
 
 outputFileName = "output.txt"
@@ -8,30 +9,35 @@ repositoryURL = None
 user = None
 password = None
 workspace = None
-stream = None
-baseline = "_BCxObPNyEeKb7fdMNGs5Yw" #rc-1306
+streams = None
+mainStream = None
 firstApplicationBaseLine = "Initial" + Shell.spaceSeparator + "CVS" + Shell.spaceSeparator + "Import"
 firstBaseLine = "Initial" + Shell.spaceSeparator + "CVS" + Shell.spaceSeparator + "Migration"
 
 
 def init():
-    Shell.execute("lscm set component -r " + repositoryURL + " -b " + firstApplicationBaseLine + " " + workspace + " stream " + stream + " BP_Application BP_Application_UnitTest")
-    Shell.execute("lscm set component -r " + repositoryURL + " -b " + firstBaseLine + " " + workspace +" stream " + stream + " BT_Frame_Installer BT_Frame_Server BT_Frame_UnitTest BX_BuildEnvironment")
+    Shell.execute(
+        "scm set component -r " + repositoryURL + " -b " + firstApplicationBaseLine + " " + workspace + " stream " + mainStream + " BP_Application BP_Application_UnitTest")
+    Shell.execute(
+        "scm set component -r " + repositoryURL + " -b " + firstBaseLine + " " + workspace + " stream " + mainStream + " BT_Frame_Installer BT_Frame_Server BT_Frame_UnitTest BX_BuildEnvironment")
 
 def readConfig():
     config = configparser.ConfigParser()
     config.read("config.ini")
-    global user, password, workspace, repositoryURL, stream
+    global user, password, workspace, repositoryURL, streams, mainStream
     generalSection = config['General']
     user = generalSection['User']
     password = generalSection['Password']
     workspace = generalSection['WorkspaceName']
     repositoryURL = generalSection['Repo']
-    stream = generalSection['Stream']
-
+    mainStream = generalSection['Stream']
+    streams = []
+    for stream in config['Migration']['Streams'].split(","):
+        streams.append(stream.strip())
 
 
 
 readConfig()
 #init()
-#RTC.acceptChangesFromBaseLine(baseline)
+handler = ImportHandler(workspace, repositoryURL)
+handler.acceptChangesFromStreams(streams)
