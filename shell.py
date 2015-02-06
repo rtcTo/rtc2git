e7bbda2a2184e1e8df6ee24e9f8423576605bdf9
Manuel <wtfjoke@livenet.ch>
2015-02-06 17:26:07 +0100

    replace spaces including method getcommands are not necessary when shell=true

--- shell.py
+++ shell.py
@@ -1,12 +1,11 @@
 import sys
+import shlex
+import json
+
 from subprocess import call
 from subprocess import check_output
 
-spaceSeparator = "****"
-
-
-def execute(commandtoexecute, outputfile=None, openmode="w"):
-    command = getcommands(commandtoexecute)
+def execute(command, outputfile=None, openmode="w"):
     if not outputfile:
         call(command, shell=True)
     else:
@@ -14,8 +13,7 @@ def execute(commandtoexecute, outputfile=None, openmode="w"):
             call(command, stdout=file, shell=True)
 
 
-def getoutput(commandtoexecute):
-    command = getcommands(commandtoexecute)
+def getoutput(command):
     outputasbytestring = check_output(command, shell=True)
     output = outputasbytestring.decode(sys.stdout.encoding).splitlines()
     strippedlines = []
@@ -26,14 +24,7 @@ def getoutput(commandtoexecute):
     return strippedlines
 
 
-def getcommands(command):
-    commands = []
-    for commandswithspaces in command.split(' '):
-        commands.append(replacespaces(commandswithspaces))
-    return commands
-
-
-def replacespaces(stringwithspaces):
-    if stringwithspaces.__contains__(' '):
-        stringwithspaces = stringwithspaces.replace(' ', spaceSeparator)
-    return stringwithspaces
+def quote(stringtoquote):
+    stringtoquote = stringtoquote.replace('\"', "'")  # replace " with '
+    quotedstring = '\"' + stringtoquote + '\"'
+    return quotedstring
