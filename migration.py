0a7d62a33d45efe085ea90e847fa30bd142e4ee0
Manuel <wtfjoke@livenet.ch>
2015-02-17 20:21:09 +0100

    extract method acceptchangesfromstreams into migration#startmigration

--- migration.py
+++ migration.py
@@ -3,7 +3,9 @@ import shutil
 
 from rtcFunctions import ImportHandler
 from gitFunctions import Initializer
-import config
+from gitFunctions import Commiter
+import configuration
+import shouter
 
 
 def initialize(directory):
@@ -12,18 +14,33 @@ def initialize(directory):
     os.mkdir(directory)
     os.chdir(directory)
     gitInitializer.initalize()
-    rtcHandler.initialize()
+    rtc.initialize()
     gitInitializer.initialcommitandpush()
 
 
 def resume(directory):
     os.chdir(directory)
     os.chdir(gitInitializer.clonedRepoName)
-    rtcHandler.loginandcollectstreams()
+    rtc.loginandcollectstreams()
 
 
-myConfig = config.readconfig()
-rtcHandler = ImportHandler(myConfig)
-gitInitializer = Initializer(myConfig)
-initialize(myConfig.workDirectory)
-rtcHandler.acceptchangesfromstreams()
+def startmigration():
+    initialize(config.workDirectory)
+    streamuuids = config.streamuuids
+    for streamuuid in streamuuids:
+        streamname = config.streamnames[streamuuids.index(streamuuid)]
+        git.branch(streamname)
+        componentbaselineentries = rtc.getbaselinesfromstream(streamuuid)
+        for componentBaseLineEntry in componentbaselineentries:
+            rtc.acceptchangesfrombaseline(componentBaseLineEntry)
+        shouter.shout("All changes of stream '%s' accepted" % streamname)
+        git.pushbranch(streamname)
+        rtc.setcomponentsofnextstreamtoworkspace(componentbaselineentries)
+        rtc.reloadworkspace()
+
+
+config = configuration.readconfig()
+rtc = ImportHandler(config)
+gitInitializer = Initializer(config)
+git = Commiter()
+startmigration()
