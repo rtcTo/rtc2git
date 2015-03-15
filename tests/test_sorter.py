71f6484162807a8629b4b809c4e4edc6fe016674
Manuel <wtfjoke@livenet.ch>
2015-03-15 18:20:05 +0100

    added another (simple) test

--- tests/test_sorter.py
+++ tests/test_sorter.py
@@ -25,6 +25,19 @@ class SorterTest(unittest.TestCase):
         self.assertEqual("1991-05-22", sortedchangesets[3].date)  # lastEntry firstKey
         self.assertEqual("1991-07-22", sortedchangesets[4].date)  # lastEntry secondKey
 
+    def testSortingThreeComponents_ExpectDatesInOrder(self):
+        earliestdate = "2015-01-30 09:00:15"
+        dateinbetween = "2015-01-30 09:00:16"
+        latestdate = "2015-02-30 09:00:00"
+        self.put("justAnotherKey", self.createchangeentry(latestdate))
+        self.put("anyKey", self.createchangeentry(earliestdate))
+        self.put("anyOtherKey", self.createchangeentry(dateinbetween))
+
+        sortedchangesets = sorter.tosortedlist(self.changeentriesmap)
+        self.assertEqual(earliestdate, sortedchangesets[0].date)
+        self.assertEqual(dateinbetween, sortedchangesets[1].date)
+        self.assertEqual(latestdate, sortedchangesets[2].date)
+
     def createchangeentry(self, date):
         self.changesetcounter += 1
         return ChangeEntry(self.changesetcounter, "anyAuthor", "anyEmail", date, "")
