af6fbcdeea9a5b40074400f1db92ad98a7d68083
Oti Humbel <ohumbel@gmail.com>
2015-06-22 20:59:34 +0200

    improve the doc comment

--- tests/test_ignore.py
+++ tests/test_ignore.py
@@ -5,27 +5,31 @@ class IgnoreTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
-    def splitstatusz(self, line):
+    def splitoutputofgitstatusz(self, line):
         """
-        :param line: the output line from the command 'git status -z'
+        Split the output of  'git status -z' into single files
+
+        :param line: the output line from the command
         :return: a list of all repository files with status changes
+
+        [ to add to .gitignore, each backslash has to be escaped with a backslash ]
         """
         repositoryfiles = []
-        entries = line.split(sep='\x00') # ascii 0 is the delimiter
+        entries = line.split(sep='\x00')         # ascii 0 is the delimiter
         for entry in entries:
             entry = entry.strip()
             if len(entry) > 0:
                 start = entry.find(' ')
                 if 1 <= start <= 2:
-                    repositoryfile = entry[3:] # output is formatted
+                    repositoryfile = entry[3:]   # output is formatted
                 else:
-                    repositoryfile = entry     # file on a single line (e.g. rename continuation)
+                    repositoryfile = entry       # file on a single line (e.g. rename continuation)
                 repositoryfiles.append(repositoryfile)
         return repositoryfiles
 
     def test_splitstatusz(self):
         with open('./resources/test_ignore_git_status_z.txt', 'r') as file:
-            repositoryfiles = self.splitstatusz(file.readline())
+            repositoryfiles = self.splitoutputofgitstatusz(file.readline())
             self.assertEqual(12, len(repositoryfiles))
             for repositoryfile in repositoryfiles:
                 print(repositoryfile)
