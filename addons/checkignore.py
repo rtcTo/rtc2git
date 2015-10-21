b2806452db3546f98a148900fbc27f28eab96160
Otmar Humbel <ohumbel@gmail.com>
2015-10-21 07:38:00 +0200

    a first try to check ignored files which are still there

---
+++ addons/checkignore.py
@@ -0,0 +1,14 @@
+import os
+
+if __name__ == "__main__":
+    ignoredbutexist = []
+    directory = '/home/huo/tmp/migrate_EMS/EMS'
+    os.chdir(directory)
+    ignorelines = []
+    with open('.gitignore', 'r') as gitignore:
+        ignorelines = gitignore.readlines()
+    for line in ignorelines:
+        if os.path.exists(line):
+            ignoredbutexist.append(line)
+    if len(ignoredbutexist) > 0:
+        print("The following files have been ignored in the new git repository, but still exist in the actual RTC workspace:")
